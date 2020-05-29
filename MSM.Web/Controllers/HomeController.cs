557f754d9138700c08b7f6b0b27509ff295e5d6c
James Thorpe <james@pawsforthorpe.co.uk>
2020-05-29 22:05:18 +0100

    added page to view available versions

--- MSM.Web/Controllers/HomeController.cs
+++ MSM.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -9,15 +10,28 @@ namespace MSM.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMetadataProvider _metadataProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMetadataProvider metadataProvider)
         {
             _logger = logger;
+            _metadataProvider = metadataProvider;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("AvailableVersions/{a?}/{b?}")]
+        public async Task<IActionResult> AvailableVersions(string a, string b)
+        {
+            var includeSnapshots = (a == "snapshots" || b == "snapshots");
+            var refresh = (a == "refresh" || b == "refresh");
+            
+            var versions = await _metadataProvider.GetManifestAsync(refresh);
+
+            return View(includeSnapshots ? versions.Versions : versions.Versions.Where(v => v.IsRelease));
         }
     }
 }
